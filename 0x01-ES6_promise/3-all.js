498bae6441a497c0eeb2c6c940420aa93f9c5c68
MahmoudEasa <mu01011422865@gmail.com>
2024-04-23 21:37:18 +0200

    Task 3

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,1 +1,16 @@
+import { uploadPhoto, createUser } from "./utils.js";
 
+export default async function handleProfileSignup() {
+	try {
+		const allItems = await Promise.all([uploadPhoto(), createUser()]);
+		let result = "";
+		for (const item of allItems) {
+			if (item.body) result += item.body + " ";
+			if (item.firstName) result += item.firstName + " ";
+			if (item.lastName) result += item.lastName;
+		}
+		console.log(result);
+	} catch (err) {
+		console.log("Signup system offline");
+	}
+}
