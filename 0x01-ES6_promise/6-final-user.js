ae6402fcee3b93f8f92cc2309d249ce2cdc9c30c
MahmoudEasa <mu01011422865@gmail.com>
2024-05-05 22:17:50 +0300

    Task 6

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,1 +1,24 @@
+import signUpUser from "./4-user-promise";
+import uploadPhoto from "./5-photo-reject";
 
+const handleProfileSignup = async (firstName, lastName, fileName) => {
+	try {
+		await signUpUser(firstName, lastName);
+		const value = await uploadPhoto(fileName);
+		return [
+			{
+				status: 200,
+				value: value,
+			},
+		];
+	} catch (err) {
+		return [
+			{
+				status: 400,
+				value: err,
+			},
+		];
+	}
+};
+
+export default handleProfileSignup;
