9ed49f1d8fcaf92a35cebd77ded81c10af0864cb
MahmoudEasa <mu01011422865@gmail.com>
2024-05-13 20:57:59 +0300

    Task 3

--- 0x02-ES6_classes/3-currency.js
+++ 0x02-ES6_classes/3-currency.js
@@ -1,1 +1,30 @@
+class Currency {
+	constructor(code, name) {
+		this.code = code;
+		this.name = name;
+	}
 
+	get code() {
+		return this._code;
+	}
+
+	set code(val) {
+		if (typeof val === "string") this._code = val;
+		else throw new TypeError("Code most be a string");
+	}
+
+	get name() {
+		return this._name;
+	}
+
+	set name(val) {
+		if (typeof val === "string") this._name = val;
+		else throw new TypeError("Name most be a string");
+	}
+
+	displayFullCurrency() {
+		return `${this._name} (${this._code})`;
+	}
+}
+
+export default Currency;
