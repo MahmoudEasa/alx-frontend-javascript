abb23cadb7ba3952d7316173bd1a581c3c93e1d7
MahmoudEasa <mu01011422865@gmail.com>
2024-05-14 23:20:11 +0300

    Task 5

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,1 +1,24 @@
+class Building {
+	constructor(sqft) {
+		this.sqft = sqft;
+		if (
+			this.constructor !== Building &&
+			this.evacuationWarningMessage === undefined
+		) {
+			throw new Error(
+				"Class extending Building must override evacuationWarningMessage"
+			);
+		}
+	}
 
+	get sqft() {
+		return this._sqft;
+	}
+
+	set sqft(val) {
+		if (typeof val === "number") this._sqft = val;
+		else throw new TypeError("sqft most be a number");
+	}
+}
+
+export default Building;
