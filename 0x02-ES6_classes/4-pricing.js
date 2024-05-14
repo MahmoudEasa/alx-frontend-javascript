02e770f535266c0ffc459abe6a3ad9036c4c6ff8
MahmoudEasa <mu01011422865@gmail.com>
2024-05-14 22:17:43 +0300

    Task 4

--- 0x02-ES6_classes/4-pricing.js
+++ 0x02-ES6_classes/4-pricing.js
@@ -1,1 +1,36 @@
+import Currency from "./3-currency";
 
+class Pricing {
+	constructor(amount, currency) {
+		this.amount = amount;
+		this.currency = currency;
+	}
+
+	get amount() {
+		return this._amount;
+	}
+
+	set amount(val) {
+		if (typeof val === "number") this._amount = val;
+		else throw new TypeError("Amount most be a number");
+	}
+
+	get currency() {
+		return this._currency;
+	}
+
+	set currency(val) {
+		if (val instanceof Currency) this._currency = val;
+		else throw new TypeError("Currency most be instance of Currency");
+	}
+
+	displayFullPrice() {
+		return `${this._amount} ${this._currency.displayFullCurrency()}`;
+	}
+
+	convertPrice(amount, conversionRate) {
+		return amount * conversionRate;
+	}
+}
+
+export default Pricing;
