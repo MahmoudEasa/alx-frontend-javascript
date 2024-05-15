7c4a79b099610d8be0e509387cf2417ef866fe78
MahmoudEasa <mu01011422865@gmail.com>
2024-05-16 00:09:00 +0300

    Task 8

--- 0x02-ES6_classes/8-hbtn_class.js
+++ 0x02-ES6_classes/8-hbtn_class.js
@@ -1,1 +1,32 @@
+export default class HolbertonClass {
+	constructor(size, location) {
+		this.size = size;
+		this.location = location;
+	}
 
+	get location() {
+		return this._location;
+	}
+
+	set location(val) {
+		if (typeof val === "string") this._location = val;
+		else throw new TypeError("Location most be a string");
+	}
+
+	get size() {
+		return this._size;
+	}
+
+	set size(val) {
+		if (typeof val === "number") this._size = val;
+		else throw new TypeError("Size most be a number");
+	}
+
+	toString() {
+		return this._location;
+	}
+
+	valueOf() {
+		return this._size;
+	}
+}
