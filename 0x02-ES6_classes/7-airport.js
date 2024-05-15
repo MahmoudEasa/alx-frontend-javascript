5d2ff292da5f3a69af0f2d39340001e6f9e19e8a
MahmoudEasa <mu01011422865@gmail.com>
2024-05-15 23:14:27 +0300

    Taks 7

--- 0x02-ES6_classes/7-airport.js
+++ 0x02-ES6_classes/7-airport.js
@@ -1,1 +1,28 @@
+export default class Airport {
+	constructor(name, code) {
+		this.name = name;
+		this.code = code;
+	}
 
+	get code() {
+		return this._code;
+	}
+
+	set code(val) {
+		if (typeof val === "string") this._code = val;
+		else throw new TypeError("Code most be a string");
+	}
+
+	get name() {
+		return this._name;
+	}
+
+	set name(val) {
+		if (typeof val === "string") this._name = val;
+		else throw new TypeError("Name most be a string");
+	}
+
+	toString() {
+		return `[object ${this._code}]`;
+	}
+}
