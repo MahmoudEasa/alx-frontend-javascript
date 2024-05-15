664e659a237df83dc298c3a52e07a4248897c554
MahmoudEasa <mu01011422865@gmail.com>
2024-05-16 01:05:36 +0300

    Task 10

--- 0x02-ES6_classes/10-car.js
+++ 0x02-ES6_classes/10-car.js
@@ -1,1 +1,17 @@
+const cloneCarSymbol = Symbol("cloneCar");
 
+export default class Car {
+	constructor(brand, motor, color) {
+		this._brand = brand;
+		this._motor = motor;
+		this._color = color;
+	}
+
+	[cloneCarSymbol]() {
+		return new this.constructor(this._brand, this._motor, this._color);
+	}
+
+	cloneCar() {
+		return this[cloneCarSymbol]();
+	}
+}
