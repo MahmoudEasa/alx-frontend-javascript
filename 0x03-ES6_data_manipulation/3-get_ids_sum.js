95c155b52fb00b4e8530ff79a1dd0ff3f1ee6e16
MahmoudEasa <mu01011422865@gmail.com>
2024-05-17 00:17:07 +0300

    Task 3

--- 0x03-ES6_data_manipulation/3-get_ids_sum.js
+++ 0x03-ES6_data_manipulation/3-get_ids_sum.js
@@ -1,1 +1,6 @@
-
+export default (listOfStudents) => {
+	if (!Array.isArray(listOfStudents)) return [];
+	return listOfStudents.reduce((accumulator, currentValue) => {
+		return accumulator + currentValue.id;
+	}, 0);
+};
