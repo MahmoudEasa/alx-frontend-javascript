553e758e068bc2466e518f6b2abb8e05afe10f3e
MahmoudEasa <mu01011422865@gmail.com>
2024-05-17 22:30:09 +0300

    Task 4

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,1 +1,11 @@
-
+export default (getListStudents, city, newGrades) => {
+	return getListStudents.filter((e) => {
+		if (e.location === city) {
+			e.grade = "N/A";
+			newGrades.map((el) => {
+				if (e.id === el.studentId) e.grade = el.grade;
+			});
+			return e;
+		}
+	});
+};
