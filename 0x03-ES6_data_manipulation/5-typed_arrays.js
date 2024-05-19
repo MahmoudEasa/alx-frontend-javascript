ca7166cb446a317161b538f952916eccbcbc3fe9
MahmoudEasa <mu01011422865@gmail.com>
2024-05-19 20:53:34 +0300

    Task 5

--- 0x03-ES6_data_manipulation/5-typed_arrays.js
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -1,1 +1,11 @@
-
+export default (length, position, value) => {
+	try {
+		const buffer = new ArrayBuffer(length);
+		const view = new Int8Array(buffer);
+		view[position] = value;
+		return new DataView(buffer);
+	} catch (err) {
+		console.log(err);
+		throw new Error("Position outside range");
+	}
+};
