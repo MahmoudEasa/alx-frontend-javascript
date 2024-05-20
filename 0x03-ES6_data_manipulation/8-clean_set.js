49049ecb6211b22eb09f00bd6ba902dd3ecb9b0b
MahmoudEasa <mu01011422865@gmail.com>
2024-05-20 23:47:19 +0300

    Task 8

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,1 +1,9 @@
-
+export default (set, startString) => {
+	if (!startString) return "";
+	const result = [];
+	set.forEach((element) => {
+		if (element.startsWith(startString))
+			result.push(element.slice(startString.length));
+	});
+	return result.join("-");
+};
