4be6705a5c85d5ad220c63678e1f169f3b849c55
MahmoudEasa <mu01011422865@gmail.com>
2024-05-21 23:35:14 +0300

    Tadk 100

--- 0x03-ES6_data_manipulation/100-weak.js
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -1,1 +1,7 @@
+export const weakMap = new WeakMap();
 
+export const queryAPI = (endpoint) => {
+	const numOfQueries = weakMap.get(endpoint) || 0;
+	if (numOfQueries >= 5) throw new Error("Endpoint load is high");
+	weakMap.set(endpoint, numOfQueries + 1);
+};
